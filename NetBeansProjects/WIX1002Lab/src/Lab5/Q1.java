2dc469b98696117fcd3774a205987a0c46ff123d
cheiw256 <chiew256@gmail.com>
2021-12-26 16:12:48 +0800

    Update Lab 5

--- NetBeansProjects/WIX1002Lab/src/Lab5/Q1.java
+++ NetBeansProjects/WIX1002Lab/src/Lab5/Q1.java
@@ -5,10 +5,35 @@
  */
 package Lab5;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author chiew256
  */
 public class Q1 {
-    
+    public static void main(String[] args) {
+        Scanner input= new Scanner (System.in);
+        Random rnd= new Random();
+        System.out.println("Please enter the number of students");
+        int N = input.nextInt();
+        int [] score = new int [N];
+        double sum =0;
+        for (int i=0; i<N; i++){
+            score [i] = rnd.nextInt(101);
+            sum += score[i];
+            System.out.println("The score is " + score[i]);
+        }
+        double average = sum / N;
+
+        //this one is basically sorting the score array to find the min and max value
+        //ps: this is made by me 1 year ago, seems like that time I was very lazy :P, sry for that
+        Arrays.sort(score);
+
+        System.out.println("The lowest score is " + score[0]);
+        System.out.println("The highest score is " + score [N-1]);
+        System.out.printf("The average score is %.2f" , average);
+    }
 }
--- NetBeansProjects/WIX1002Lab/src/Lab5/Q2.java
+++ NetBeansProjects/WIX1002Lab/src/Lab5/Q2.java
@@ -5,10 +5,33 @@
  */
 package Lab5;
 
+import java.util.Random;
+
 /**
  *
  * @author chiew256
  */
 public class Q2 {
-    
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int number_to_be_generated = 10;
+        int [] num = new int [number_to_be_generated];
+        for (int i=0;i<num.length;i++){
+            num [i]=rnd.nextInt(21);
+            //check all the numbers generated before this
+            for (int n=0;n<i;n++){
+                if(num[n]==num[i]){
+                    //if duplicated, generate again
+                    i--;
+                    break;
+                }
+            }
+        }
+
+        //print it out
+        System.out.println("The " + number_to_be_generated + " numbers are ");
+        for (int i=0;i<num.length;i++)
+            System.out.print(num[i] + " ");
+
+    }
 }
